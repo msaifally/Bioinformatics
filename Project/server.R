7bfdf97e5738d8009e0221776f59d472ad2c8b59
msaifally <saifally@outlook.com>
2016-12-30 17:34:02 +0400

    Quality Control
    Quality Control plot added in second panel(prepocessing)

--- Project/server.R
+++ Project/server.R
@@ -14,12 +14,12 @@ library(R.utils)
 
 
 
-
+######maximum number of upload size
 options(shiny.maxRequestSize=500*1024^2)
 shinyServer(function(input, output,session) {
   celfiles=NULL
   count<-0;
-  
+  #########INput file in working directory
   filedata <- reactive({
     infile <- input$file
     if (is.null(infile)){
@@ -33,7 +33,7 @@ shinyServer(function(input, output,session) {
     
   })
   
-  
+ ####### Input and reformat phenodata file
   filepheno <- reactive({
     infile <- input$pheno
     if (is.null(infile)){
@@ -64,18 +64,14 @@ shinyServer(function(input, output,session) {
     output$fileTable<-renderTable({filedata()},bordered = TRUE)    
     
   })
- 
-  
-  
-  if (count==2)
-  {
-  
-    
-  }
   
   
   
- 
+
+  
+  
+  
+  
   
   
   
@@ -89,56 +85,75 @@ shinyServer(function(input, output,session) {
   ############################################################################################################
   #button prepocesiing
   observeEvent(input$btnPrepocessing,{
-    # Move to results page
+ # Move to results page
     updateNavbarPage(session, "navbar", selected="tabPreprocessing")
-    
+
     withProgress(message = "Status", detail = "fetching data", value = 0, {
       Sys.sleep(3)
-      
+
       incProgress(0.1, detail = "Loading required library")
+      library(affy)
       library(simpleaffy)
       library(RColorBrewer)
       library(affyPLM)
-      
-      
+
+
       incProgress(0.4, detail = "Normalising")
-       celfiles <- read.affy(covdesc="phenodata.txt", path="data")
-       celfiles.gcrma <- gcrma(celfiles)
+      celfiles <- read.affy(covdesc=input$pheno$name, path="data")
+      celfiles.gcrma <- gcrma(celfiles)
+
+
+       nsFilter(celfiles.gcrma, require.entrez=FALSE, remove.dupEntrez=FALSE)
+
+
+
+
       # # set colour palette
-       
-       incProgress(0.4, detail = "Plotting Boxplot ")
-       
-       cols <- brewer.pal(8, "Set1")
-       # plot a boxplot of unnormalised intensity values
-       
-       output$boxPlot1 <- renderPlot({
-         boxplot(celfiles, col=cols)
-       })
-       
-       output$boxplot2 <- renderPlot({
-         boxplot(celfiles.gcrma, col=cols)
-       })
-       # the boxplots are somewhat skewed by the normalisation algorithm
-       # and it is often more informative to look at density plots
-        # Plot a density vs log intensity histogram for the unnormalised data
-        incProgress(0.7, detail = "Plotting Histogram ")
-        
-        output$histogram1 <- renderPlot({
-          hist(celfiles, col=cols)
-        })
-       # Plot a density vs log intensity histogram for the normalised data
-        output$histogram2 <- renderPlot({
-          hist(celfiles.gcrma, col=cols)
-        })
-     
+
+
+      incProgress(0.4, detail = "Plotting Boxplot ")
+
+      cols <- brewer.pal(8, "Set1")
+      # plot a boxplot of unnormalised intensity values
+
+      output$boxPlot1 <- renderPlot({
+        boxplot(celfiles, col=cols)
+      })
+
+      output$boxplot2 <- renderPlot({
+        boxplot(celfiles.gcrma, col=cols)
+      })
       
-    }
+      
+      # the boxplots are somewhat skewed by the normalisation algorithm
+      # and it is often more informative to look at density plots
+      # Plot a density vs log intensity histogram for the unnormalised data
+      incProgress(0.7, detail = "Plotting Histogram ")
+
+      output$histogram1 <- renderPlot({
+        hist(celfiles, col=cols)
+      })
+      # Plot a density vs log intensity histogram for the normalised data
+      output$histogram2 <- renderPlot({
+        hist(celfiles.gcrma, col=cols)
+      })
+      
+      incProgress(0.8, detail = "Plotting Quality control ")
+      aqc<-qc(celfiles)
+
+      incProgress(0.8, detail = "Plotting Quality control 2")
+      output$qualitycontrolplot <- renderPlot({
+        plot(aqc)
+     })
     
-    )
-    toggle(selector = "#navbar li a[data-value=tabPreprocessing]")
     
+  
+    })#withprogress ends here
     
-  })
+     toggle(selector = "#navbar li a[data-value=tabPreprocessing]")
+     
+     
+   })
   
   ############################################################################################################
   #button geneselection
@@ -155,11 +170,45 @@ shinyServer(function(input, output,session) {
       incProgress(0.5, detail = "part two")
       Sys.sleep(2)
       
-     toggle(selector = "#navbar li a[data-value=tabGeneSelection]")
+      toggle(selector = "#navbar li a[data-value=tabGeneSelection]")
       
     })
     
   })
+  
+  ############################################################################################################
+  #SELECT annotation data
+  x <- reactive({
+    
+    if(input$select == 'hgu133plus2'){
+      
+      source("https://bioconductor.org/biocLite.R")
+      biocLite("hgu133plus2.db")
+      
+    } 
+    else if(input$select == 'hgu95av2'){
+      source("https://bioconductor.org/biocLite.R")
+      biocLite("hgu95av2.db")
+    }
+  })   
+  output$state <- renderText(x())
+  ############################################################################################################
+  #Submit gene selection method
+  r <- eventReactive(input$suBmit,{
+    
+    
+    if(input$radio == 'SAM'){
+      source("https://bioconductor.org/biocLite.R")
+      biocLite("hgu95av2.db")
+    }
+    if(input$radio == 'LIMMA'){
+      source("https://bioconductor.org/biocLite.R")
+      biocLite("hgu133plus2.db")
+      
+    }
+    
+  })
+  output$lol <- renderText(r())
   
   ############################################################################################################
   #button plot
--- Project/ui.R
+++ Project/ui.R
@@ -11,48 +11,87 @@ library(shiny)
 library(shinyjs)
 library(R.utils)
 
-shinyUI(fluidPage(title="Testing", useShinyjs(),
+shinyUI(fluidPage(title="MICROARRAY", useShinyjs(),
+                  includeCSS("style.css"),
                   navbarPage(id = "navbar",inverse=TRUE,title=div(img(src="logo.jpg")),
                              #panel Home
-                             tabPanel(title ="Home",
+                             tabPanel(title ="Home",icon = icon("home"),
                                       #sidebar panel
-                                      sidebarPanel(
+                                      sidebarPanel(id = "sidebar",
                                         fileInput("file","Input zip cel files"),
                                         fileInput("pheno","Input phenodata file")
                                       )#sidebar panel ends here.
                                       #main panel 
-                                      ,mainPanel(
+                                      ,mainPanel(id = "mainpanel",
+                                                 h3( strong("SmArray"), "is a web-based application supporting analysis,
+                                             visualization, and stratification of large genomic data, 
+                                             particularly microarray data."),
+                                                 img(src='rsz_myimage.jpg', align = "center"),
+                                                 
                                         tabsetPanel(id="pa",
                                                     tabPanel("FILE", value="b",tableOutput("fileTable")),
                                                     tabPanel("Phenodata",value="a" ,tableOutput("phenoTable"))
-
+                                                    
                                         )
                                       )#main panel ends here
                                       
                                       
-                                      ,  tags$footer(actionButton("btnPrepocessing", "Prepocessing",class="btn-info",icon = icon("mail-forward")),
-                                                     align = "center", 
-                                                     style = "position:absolute;
-                                                     bottom:0;
-                                                     width:100%;
-                                                     height:50px;   /* Height of the footer */
-                                                     color: white;
-                                                     padding: 10px;
-                                                     background-color: white;
-                                                     z-index: 1000;")
+                                      ,  tags$footer(actionButton("btnPrepocessing", "Prepocessing",class="btn-secondary",icon = icon("mail-forward")),
+                                                     align = "center"
+                                                    )
                                       ),#panel home ends here
                              
                              #panel Prepocessing 
-                             tabPanel(title = "Prepocessing",value= "tabPreprocessing",actionButton("btnGeneSelection", "Gene Selection"),
+                             tabPanel(title = "Prepocessing",value= "tabPreprocessing",
+                                      icon = icon("cogs"),
                                       tabsetPanel(
-                                        tabPanel(title="Boxplot","Before Normalisation",plotOutput("boxPlot1"),
-                                                 "After Normalisation", plotOutput("boxplot2")),
-                                        tabPanel(title="Histogram","Before Normalisation",plotOutput("histogram1"),
-                                                 "After Normalisation", plotOutput("histogram2"))
-                                      )),
+                                        tabPanel(style = "background-color: #ffffff;",title="Boxplot","Before Normalisation",plotOutput("boxPlot1"),
+                                                 tags$hr(style="color = #000000"),
+                                                  "After Normalisation", plotOutput("boxplot2")),
+                                        tabPanel(style = "background-color: #ffffff;",title="Histogram","Before Normalisation",plotOutput("histogram1"),
+                                                 "After Normalisation", plotOutput("histogram2")),
+                                        tabPanel(style = "background-color: #ffffff;",title="Quality Control","Stats",plotOutput("qualitycontrolplot"),
+                                                 tags$hr(style="color = #000000"))
+                                      ),
+                                      tags$footer(actionButton("btnGeneSelection", "Gene Selection",class="btn-secondary",icon = icon("mail-forward")),
+                                                  align = "center")
+                                      ),
                              #panel Gene- Selection
-                             tabPanel("Gene Selection", value = "tabGeneSelection",actionButton("btnPlot", "Plot")),
+                             tabPanel("Gene Selection",icon = icon("line-chart"), value = "tabGeneSelection",
+                                      sidebarPanel( id = "sidebar",
+                                                    h5("GENE SELECTION ANALYSIS"),
+                                                    tags$hr(),
+                                                    selectInput("select", h5("Annotation:"), 
+                                                                
+                                                                c("SELECT","hgu133plus2", "hgu95av2"),selected = "SELECT"
+                                                    ),
+                                                    verbatimTextOutput("state"),
+                                                    tags$hr(),
+                                                    radioButtons("radio", label = h5("Feature Selection method"),
+                                                                 c("SAM", "LIMMA") 
+                                                    ),
+                                                    
+                                                    verbatimTextOutput("lol"),
+                                                    tags$hr(),
+                                                    actionButton("suBmit","SUBMIT")
+                                                    
+                                      ),
+                                      mainPanel(id = "mainpanel",
+                                                h3("Gene Selection allows to differentially expressed genes for microarray label data"),
+                                                br(),
+                                                tabsetPanel(
+                                                  tabPanel(title="Table","Selected genes Table",renderTable("table")),
+                                                  
+                                                  tabPanel(title="Heatmap","Data visualization",plotOutput("heatmap")
+                                                           
+                                                  )
+                                                )
+                                                
+                                                
+                                      ), tags$footer(actionButton("btnclassification", "Classification",class="btn-secondary",icon = icon("mail-forward")),
+                                                     align = "center")
+                                      ),
                              #Panel Plot
-                             tabPanel("Plot", value = "tabPlot",actionButton("btnBack", "Back"))
+                             tabPanel("Classification",icon = icon("bar-chart"), value = "tabPlot",actionButton("btnBack", "Back"))
                              
                                       )))
